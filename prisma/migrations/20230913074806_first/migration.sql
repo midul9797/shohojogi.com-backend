3638cb788843f1d83c4bdeebb8eda1ab181cdddb
midul9797 <mrmnso9797@gmail.com>
2023-09-13 13:52:55 +0600

    package.json script build updated

--- prisma/migrations/20230913074806_first/migration.sql
+++ prisma/migrations/20230913074806_first/migration.sql
@@ -33,7 +33,7 @@ CREATE TABLE "books" (
     "author" TEXT NOT NULL,
     "price" DOUBLE PRECISION NOT NULL,
     "genre" TEXT NOT NULL,
-    "publicationDate" TIMESTAMP(3) NOT NULL,
+    "publicationDate" TEXT NOT NULL,
     "categoryId" TEXT NOT NULL,
 
     CONSTRAINT "books_pkey" PRIMARY KEY ("id")
